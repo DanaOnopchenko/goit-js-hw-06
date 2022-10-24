67a4db50c392dc3bf9b4a6ea36b7dd55da136ef7
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-10-25 00:58:23 +0300

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const ulElem = document.querySelector('#categories');
+// console.log(ulElem)
+const itemElem = ulElem.querySelectorAll('.item');
+// console.log(itemElem)
+console.log(`Number of categories: ${itemElem.length}`);
+// console.log(itemElem[0])
