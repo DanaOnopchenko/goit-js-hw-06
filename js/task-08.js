d8d7e222869aca429d8086065f9a29f804e8e284
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-11-05 03:03:44 +0200

    Task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formRefs = document.querySelector(".login-form");
+formRefs.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) { 
+    event.preventDefault();
+    const { 
+          elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") { 
+        return alert("Всі поля повинні бути заповнені!")
+    }
+    console.log(`email: ${email.value}, password: ${password.value}`);
+    event.currentTarget.reset()
+    
+    }
 
