66eea1167c439f2b190c4a31562764d8a655dba3
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-11-05 04:25:14 +0200

    Task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRefs = document.querySelector("body");
+const spanRefs = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+// console.log(btnChangeColor)
+btnChangeColor.addEventListener('click', onRandomColor);
+
+function onRandomColor() { 
+  bodyRefs.style.backgroundColor = getRandomHexColor();
+  spanRefs.textContent = bodyRefs.style.backgroundColor;
+}
